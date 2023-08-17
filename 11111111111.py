bf45d2fd42e4a0a669fd9dd1ef6fbaaffa0ccc4f
myukzzz <68174757+myukzzz@users.noreply.github.com>
2023-08-17 21:02:28 +0800

    Add files via upload

---
+++ 11111111111.py
@@ -0,0 +1,10 @@
+f = open("VGclasses.txt", "r")
+class_names=[]
+class_list = f.readlines()
+for classes in class_list:
+    classes=classes.strip('\n')
+    class_names.append(classes)
+
+print("111111111111111111")
+
+
